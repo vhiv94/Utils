d085183de3897f57153ff3dbadcb949eb9136d6a
Velton <vhiv94@gmail.com>
2025-02-13 14:07:31 -0600

    Add color data format transformers
    Add two helper functions that take in a comma-separated RGB triplet and convert it to Hex format, with the hash, and vice versa, with or without the hash.

---
+++ RGBToHex.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void convert(void)
+{
+	int r = 0;
+	int g = 0;
+	int b = 0;
+
+	scanf_s("%d, %d, %d", &r, &g, &b);
+	while (getchar() != '\n');
+
+	printf_s("#%02X%02X%02X\n", r, g, b);
+}
+
+int main(void)
+{
+	char y = 0;
+	do
+	{
+		convert();
+		printf_s("Again? (Y/N)\n");
+		y = getchar();
+	} while (y == 'y' || y == 'Y');
+	exit(0);
+	return 0;
+}
